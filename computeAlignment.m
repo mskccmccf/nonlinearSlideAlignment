c94b0e95bee1364eea2cf3a858f99dc297326f90
Anthony Santella <anthony.santella@gmail.com>
2022-01-20 17:12:15 -0500

    updated normalization
    Normalization now applied bf linear alignment (still updated rigid) unused but present internal parameter to compute linear at lower resolution

--- computeAlignment.m
+++ computeAlignment.m
@@ -15,29 +15,49 @@ bk=imerode(medfilt2(smallresmoving_IF),offsetstrel('ball',5,5));
 %sometime and keeping treatment uniform.
 smallresmoving_IF=max(smallresmoving_IF-bk,0);
 
+%normalize first hope this helps with unstable linear alignment 
+smallresmoving_IF=min(1,single(smallresmoving_IF)./single(prctile(max(smallresmoving_IF),70)));
+smallresfixed_IH=min(1,smallresfixed_IH./prctile(max(smallresfixed_IH),70));
+
+%currently not using linear at lower scale option but it is still
+%functional
+scalebtwlinnon=1;
+%align linear at half scale of 
+smallresfixed_IHlin=imresize(smallresfixed_IH,1/scalebtwlinnon);
+smallresmoving_IFlin=imresize(smallresmoving_IF,1/scalebtwlinnon);
+
 %second step linear alignment
 %one of two methods based on alignmode flag
 if (alignmode)
-    tform=imregcorr(smallresmoving_IF,smallresfixed_IH,'rigid');
+    %would this be better limited to rigid or translation, which should be
+    %sufficentl
+    tform=imregcorr(smallresmoving_IFlin,smallresfixed_IHlin,'rigid');
 else
     [optimizer,metric]=imregconfig('multimodal');
-    tform=imregtform(smallresmoving_IF,smallresfixed_IH,'rigid',optimizer,metric);
-end
-Rfixed = imref2d(size(smallresfixed_IH));
-ifcmovingrlin = imwarp(smallresmoving_IF,tform,'OutputView',Rfixed);
-if (visualize)
-    imshowpair(smallresfixed_IH,ifcmovingrlin*10);title('linear alingment');
+    tform=imregtform(smallresmoving_IFlin,smallresfixed_IHlin,'rigid',optimizer,metric); %was affine similarity is better but still off
 end
 
+%doing linear at half scale of nonlinear
+tform.T(3,1:2)=tform.T(3,1:2)*scalebtwlinnon;
+
+
+Rfixed = imref2d(size(smallresfixed_IH));
+%moving with linear applied for nonlinear
+ifcmovingrlin = imwarp(smallresmoving_IF,tform,'OutputView',Rfixed);
+if (visualize)
+    figure; imshowpair(smallresfixed_IH,ifcmovingrlin*10);title('linear alingment');
+end
+
+%{
 %normalize bf nonlinear, not sure this actually helps.
 ifcmovingrlin_n=min(1,single(ifcmovingrlin)./single(prctile(max(ifcmovingrlin),70)));
 smallresfixed_IH_n=min(1,smallresfixed_IH./prctile(max(smallresfixed_IH),70));
-
+%}
 %third step nonlinear alignment
-[warpfield,smallregisteredifc]=imregdemons(ifcmovingrlin_n,smallresfixed_IH_n,[500 500 100 10  ],'PyramidLevels',4,'AccumulatedFieldSmoothing',1.3);
+[warpfield,smallregisteredifc]=imregdemons(ifcmovingrlin,smallresfixed_IH,[500 500 100 10  ],'PyramidLevels',4,'AccumulatedFieldSmoothing',1.3);
 
 if (visualize)
-    figure;imshowpair(smallresfixed_IH_n,min(1,smallregisteredifc));title('1 to 16 nonlinear alignment 4level norm_v2');
+    figure;imshowpair(smallresfixed_IH,min(1,smallregisteredifc));title('1 to 16 nonlinear alignment 4level norm_v2');
     figure;
     visimage=cat(3,smallresfixed_IH,smallregisteredifc,abs(max(warpfield,[],3)*2));
     imagesc(visimage);
